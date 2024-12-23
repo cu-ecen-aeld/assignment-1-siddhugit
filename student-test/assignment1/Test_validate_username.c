c91c3306e92fa1a57e1341f8f9e68d543c79dd24
siddharth singh <siddharth.singh@blockeng.com>
2024-12-23 17:33:16 -0500

    Completed assignment-1

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,9 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char * userName = my_username();
+    const char * confUserName = malloc_username_from_conf_file();
+    //printf("my_username() = %s\n",userName);
+    //printf("malloc_username_from_conf_file() = %s\n",confUserName);
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(userName,confUserName,"my_username and malloc_username_from_conf_file does not match");
 }
